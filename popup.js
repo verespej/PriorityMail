74120e20b76927c159c6ab1fad171896883253f7
Hakon Verespej <hakon@Hakons-MacBook-Air.local>
2013-01-20 19:28:30 -0800

    Make board sizes dynamic and resizeable Also do some cleanup including variable renaming

--- popup.js
+++ popup.js
@@ -1,28 +1,33 @@
 //Fire a request to Gmail RSS feed
 var req = new XMLHttpRequest();
 var data = "not yet";
-req.open(
-    "GET",
-    "https://mail.google.com/mail/feed/atom",
-    true);
 req.onload = showMails;
+req.open(
+  "GET",
+  "https://mail.google.com/mail/feed/atom",
+  true);
 req.send(null);
 
 function showMails() {
-  var allEmailsInFeed = req.responseXML.getElementsByTagName("title");
-
-  for (var i = 0; email = allEmailsInFeed[i]; i++) {
-	$('#masterDiv').append('<div class="card">' + email.textContent + '</div>');
+  var emails = req.responseXML.getElementsByTagName("title");
+  for (var i = 0; i < emails.length; i++) {
+    $("#unassignedEmailPile").append("<div class='email' id='email" + i + "'>" + emails[i].textContent + "</div>");
   }
-  refreshCards();
+
+  $(".email").each(function(i) {
+    this.draggable = true;
+    this.ondragstart = function(e) { drag(e); };
+  });
 }
 
 function allowDrop(e) {
   e.preventDefault();
 }
+
 function drag(e) {
   e.dataTransfer.setData("Text", e.target.id);
 }
+
 function drop(e) {
   // Receive the id of the element and append the element to the dest
   e.preventDefault();
@@ -31,22 +36,15 @@ function drop(e) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-refreshCards();
+  refresh();
 });
 
-
-function refreshCards() {
- $(".board").each(function(i) {
+function refresh() {
+  var pileWidth = 100 * (1 / $(".emailPile").length) + "%";
+  $(".emailPile").each(function(i) {
     this.ondrop = function(e) { drop(e); };
     this.ondragover = function(e) { allowDrop(e); };
+    this.width = pileWidth;
   });
-  var cardCount = 0;
-
-  $(".card").each(function(i) {
-    this.id = "card" + cardCount.toString();
-    this.draggable = true;
-    this.ondragstart = function(e) { drag(e); };
-    cardCount++;
-  });
-
 }
+
